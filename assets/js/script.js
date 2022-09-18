8d10f26844df1d98dc9a2f46b150c4d7f5016900
mvv1790 <mvv1790@gmail.com>
2022-09-18 13:51:50 +0000

    Created a game sprite and a simple animation

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,56 @@
 
+let gameCanvas = document.getElementById('gameCanvas');
 
-function drawSprites() {
+const ctx = gameCanvas.getContext ('2d');
 
-    if (!canvas.getContext) {
-        return;
+
+
+window.addEventListener('keydown',(event) => {
+    console.log(event.key)
+})
+
+class Sprite {
+    constructor({position, velocity}) {
+        this.position = position
+        this.velocity = velocity
+        this.height = 10
+        this.width = 10
     }
+    crateSprite(){
+        ctx.fillStyle = '#F9DC5C'
+        ctx.fillRect(this.position.x, this.position.y, this.width, this.height)
+        
+    }
+    refreshAnimation() {
+        this.crateSprite()
+        this.position.x += this.velocity.x
 
-    const canvas =  document.querySelector ('#canvas');
-    const ctx = canvas.getContext ('2d');
+        if (this.position.x + this.width + this.velocity.x >= gameCanvas.width) {
+            this.velocity.x = 0
+        }
 
-    
-    ctx.fillStyle = '#F9DC5C';
-    ctx.fillRect(100, 100, 150, 100);
-
+    }
 }
 
-drawSprites();
- 
+const player = new Sprite ({
+    position: {
+    x:15,
+    y:120
+},
+    velocity: {
+    x:1,
+    y:0, 
+    }
+})
+
+player.crateSprite()
+
+console.log(player)
+function animations() {
+    ctx.fillStyle ='black'
+    ctx.fillRect(0, 0, gameCanvas.width, gameCanvas.height)
+    window.requestAnimationFrame(animations)
+    player.refreshAnimation()
+}
+
+animations()
