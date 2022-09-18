294e25b2909e4b2335188d91fbef325f139bac21
mvv1790 <mvv1790@gmail.com>
2022-09-18 22:37:59 +0000

    Created and added player map movement keys

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,46 @@
 
 let gameCanvas = document.getElementById('gameCanvas');
 
-const ctx = gameCanvas.getContext ('2d');
+let ctx = gameCanvas.getContext ('2d');
 
+const gravity = 0.6
 
-
+//Player movement keyboard 
 window.addEventListener('keydown',(event) => {
+    switch (event.key){
+        case'd':
+        player.velocity.x = 1
+        break
+        case 'a':
+        player.velocity.x = -1
+        break
+        case'w':
+        player.velocity.y = -5
+        break
+        case's':
+        player.velocity.y = 1
+        break
+
+    }
+    console.log(event.key)
+})
+
+window.addEventListener('keyup',(event) => {
+    switch (event.key){
+        case'd':
+        player.velocity.x = 0
+        break
+        case 'a':
+        player.velocity.x = 0
+        break
+        case'w':
+        player.velocity.y = 0
+        break
+        case's':
+        player.velocity.y = 0
+        break
+
+    }
     console.log(event.key)
 })
 
@@ -13,21 +48,37 @@ class Sprite {
     constructor({position, velocity}) {
         this.position = position
         this.velocity = velocity
-        this.height = 10
+        this.height = 15
         this.width = 10
+        this.attackBox = {
+            position: this.position,
+            width: 10,
+            height: 8
+        }
     }
     crateSprite(){
         ctx.fillStyle = '#F9DC5C'
         ctx.fillRect(this.position.x, this.position.y, this.width, this.height)
         
+        //attack box
+        ctx.fillRect (
+            this.attackBox.position.x,
+            this.attackBox.position.y, 
+            this.attackBox.width,
+            this.attackBox.height
+        )
     }
     refreshAnimation() {
         this.crateSprite()
         this.position.x += this.velocity.x
+        this.position.y += this.velocity.y
 
         if (this.position.x + this.width + this.velocity.x >= gameCanvas.width) {
             this.velocity.x = 0
         }
+        if (this.position.y + this.width + this.velocity.y >= gameCanvas.height) {
+            this.velocity.y = 0
+        } else this.velocity.y += gravity
 
     }
 }
@@ -38,7 +89,7 @@ const player = new Sprite ({
     y:120
 },
     velocity: {
-    x:1,
+    x:0,
     y:0, 
     }
 })
@@ -47,9 +98,9 @@ player.crateSprite()
 
 console.log(player)
 function animations() {
+    window.requestAnimationFrame(animations)
     ctx.fillStyle ='black'
     ctx.fillRect(0, 0, gameCanvas.width, gameCanvas.height)
-    window.requestAnimationFrame(animations)
     player.refreshAnimation()
 }
 
